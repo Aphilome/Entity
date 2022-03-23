502830ff33563c45a3695020770abedeab3be7f3
Aura Philomena <1.katrin.1@mail.ru>
2022-03-24 02:07:26 +0300

    code conversion principles remark

--- MadEntity/MadEntity/Entities/Person.cs
+++ MadEntity/MadEntity/Entities/Person.cs
@@ -8,7 +8,7 @@ namespace MadEntity.Entities
 {
     internal class Person
     {
-        public int Id2 { get; set; }
+        public int Id { get; set; }
 
         public string? Name { get; set; }
 
